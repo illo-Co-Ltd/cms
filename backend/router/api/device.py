19a9f449c465951bd2a05603558a623f00997f29
Dev-Jahn <ajh508@naver.com>
2021-01-12 11:57:55 +0900

    Added device registration API

--- backend/router/api/device.py
+++ backend/router/api/device.py
@@ -10,36 +10,40 @@ import datetime
 device_route = Blueprint('device_route', __name__)
 
 
-@device_route.route('/tweet', methods=["GET"])
+@device_route.route('/device', methods=["GET"])
 @token_required
-def tweet_list(current_user):
-    logger.info("get Tweet List")
-    tweet_list = device_model.Tweet.query.all()
-    return jsonify([t.to_dict() for t in tweet_list])
+def list_device(current_user):
+    logger.info("Get device list")
+    device_list = device_model.Device.query.all()
+    return jsonify([d.to_dict() for d in device_list])
 
 
-@device_route.route('/tweet', methods=["POST"])
+@device_route.route('/device', methods=["POST"])
 @token_required
-def create_tweet(current_user):
-    logger.info("Tweet Post!")
+def register_device(current_user):
+    logger.info("Register new device")
     try:
         data = request.get_json()
-        title = data.get("title")
-        words = data.get("words")
-        created_at = datetime.datetime.utcnow()
-        updated_at = datetime.datetime.utcnow()
+        model = data.get("model")
+        serial = data.get("serial")
+        owner = data.get("owner")
+        created_by = data.get("created_by")
+        edited_by = data.get("edited_by")
 
         db = device_model.db
-        tweet = device_model.Tweet(
-            title=title,
-            words=words,
-            creator=current_user.username,
-            created_at=created_at,
-            updated_at=updated_at
+        device = device_model.Device(
+            model=model,
+            serial=serial,
+            owner=owner,
+            created=datetime.datetime.utcnow(),
+            created_by=created_by,
+            last_edited=datetime.datetime.utcnow(),
+            edited_by=edited_by,
+            is_deleted=False
         )
-        db.session.add(tweet)
+        db.session.add(device)
         db.session.commit()
-        return jsonify(tweet.to_dict()), 200
+        return jsonify(device.to_dict()), 200
     except Exception as e:
         logger.error(e)
-        return jsonify({'message': 'fail to save tweet'}), 200
+        return jsonify({'message': 'failed to register device'}), 200
